771717e2075dc8c188ac4876a99ed9a1bb2f0328
androgeox <geobarit@gmail.com>
2017-05-09 11:46:47 +0300

    add create, toggle, delete methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
 
+class Todo{
+  constructor(public title: string,
+              public completed: boolean = false){}
+}
+
+const todos = [
+  {
+    title: 'Изучать Java',
+    completed: true
+  },
+  {
+    title: 'Node.JS',
+    completed: false
+  },
+  {
+    title: 'Изучить Angular 2',
+    completed: false
+  }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title: string = 'My ToDo!';
+  todos: Todo[] = todos;
+  newTodoTitle: string = '';
+
+  create(){
+    let todo: Todo = new Todo(this.newTodoTitle);
+
+    this.todos.push(todo);
+    this.newTodoTitle = '';
+  }
+
+  toggle(todo: Todo){
+    todo.completed = !todo.completed;
+  }
+
+  delete(todo: Todo){
+    let index =  this.todos.indexOf(todo);
+
+    if(index > -1){
+      this.todos.splice(index, 1);
+    }
+  }
 }
